d78112a4ed25be3fb512c0496ddea444410aa034
Brendan Turner <brendanhturner@gmail.com>
2021-08-15 18:49:49 -0400

    Removed uneeded files

--- DataEngineer/PostgresSetup.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-
-try:
-    conn = psycopg2.connect(
-        "host=127.0.0.1 dbname=studentdb user=student password=student"
-    )
-except psycopg2.Error as e:
-    print("Error: Could not make connection to the Postgres database")
-    print(e)
