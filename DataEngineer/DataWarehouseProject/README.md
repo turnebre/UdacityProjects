b2e978d9a8594d31b08fd0c2acc3f3bb30eff672
Brendan <brendanhturner@gmail.com>
2021-09-15 00:38:57 -0400

    cluster configuration

---
+++ DataEngineer/DataWarehouseProject/README.md
@@ -0,0 +1,3 @@
+## Configuration
+
+### Ensure that correct security group, IAM role, secret keys, and public availablity for cluster are enabled prior to running create_tables.py
--- DataEngineer/DataWarehouseProject/sql_queries.py
+++ DataEngineer/DataWarehouseProject/sql_queries.py
@@ -3,69 +3,124 @@ import configparser
 
 # CONFIG
 config = configparser.ConfigParser()
-config.read('dwh.cfg')
+config.read("dwh.cfg")
 
 # DROP TABLES
 
-staging_events_table_drop = ""
-staging_songs_table_drop = ""
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+staging_events_table_drop = "drop table if exists staging_events"
+staging_songs_table_drop = "drop table if exists staging_songs"
+songplay_table_drop = "drop table if exists songplays"
+user_table_drop = "drop table if exists users"
+song_table_drop = "drop table if exists sings"
+artist_table_drop = "drop table if exists artists"
+time_table_drop = "drop table if exists time"
 
 # CREATE TABLES
 
-staging_events_table_create= ("""
-""")
 
-staging_songs_table_create = ("""
-""")
+staging_events_table_create = """
+CREATE TABLE IF NOT EXISTS staging_events (
+    artist text,
+    auth text,
+    firstName text,
+    gender text,
+    itemInSession int,
+    length decimal,
+    level text,
+    location text,
+    method text,
+    page text,
+    registration text,
+    sessionId int,
+    song text,
+    status int,
+    userAgent text,
+    userId int
+)
+"""
 
-songplay_table_create = ("""
-""")
+staging_songs_table_create = """
+CREATE TABLE if not exists staging_events (
+    num_songs int, 
+    artist_id text,
+    artist_latitude decimal,
+    artist_location text,
+    artist_name text,
+    song_id text,
+    title text,
+    duration decimal,
+    year int)
+"""
 
-user_table_create = ("""
-""")
+songplay_table_create = """
+"""
 
-song_table_create = ("""
-""")
+user_table_create = """
+"""
 
-artist_table_create = ("""
-""")
+song_table_create = """
+"""
 
-time_table_create = ("""
-""")
+artist_table_create = """
+"""
+
+time_table_create = """
+"""
 
 # STAGING TABLES
 
-staging_events_copy = ("""
-""").format()
+staging_events_copy = (
+    """
+"""
+).format()
 
-staging_songs_copy = ("""
-""").format()
+staging_songs_copy = (
+    """
+"""
+).format()
 
 # FINAL TABLES
 
-songplay_table_insert = ("""
-""")
+songplay_table_insert = """
+"""
 
-user_table_insert = ("""
-""")
+user_table_insert = """
+"""
 
-song_table_insert = ("""
-""")
+song_table_insert = """
+"""
 
-artist_table_insert = ("""
-""")
+artist_table_insert = """
+"""
 
-time_table_insert = ("""
-""")
+time_table_insert = """
+"""
 
 # QUERY LISTS
 
-create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
-drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+create_table_queries = [
+    staging_events_table_create,
+    staging_songs_table_create,
+    songplay_table_create,
+    user_table_create,
+    song_table_create,
+    artist_table_create,
+    time_table_create,
+]
+drop_table_queries = [
+    staging_events_table_drop,
+    staging_songs_table_drop,
+    songplay_table_drop,
+    user_table_drop,
+    song_table_drop,
+    artist_table_drop,
+    time_table_drop,
+]
 copy_table_queries = [staging_events_copy, staging_songs_copy]
-insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
+insert_table_queries = [
+    songplay_table_insert,
+    user_table_insert,
+    song_table_insert,
+    artist_table_insert,
+    time_table_insert,
+]
