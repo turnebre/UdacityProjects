4db1547b7b434ae4ce82993234a43aa616aff15f
Brendan <brendanhturner@gmail.com>
2021-09-17 22:24:54 -0400

    finished etl

--- DataEngineer/DataWarehouseProject/sql_queries.py
+++ DataEngineer/DataWarehouseProject/sql_queries.py
@@ -25,6 +25,7 @@ CREATE TABLE IF NOT EXISTS staging_events (
     firstName text,
     gender text,
     itemInSession int,
+    lastName text,
     length decimal,
     level text,
     location text,
@@ -34,16 +35,18 @@ CREATE TABLE IF NOT EXISTS staging_events (
     sessionId int,
     song text,
     status int,
+    ts bigint,
     userAgent text,
     userId int
 )
 """
 
 staging_songs_table_create = """
-CREATE TABLE if not exists staging_events (
+CREATE TABLE if not exists staging_songs (
     num_songs int, 
     artist_id text,
     artist_latitude decimal,
+    artist_longitude decimal,
     artist_location text,
     artist_name text,
     song_id text,
@@ -53,47 +56,188 @@ CREATE TABLE if not exists staging_events (
 """
 
 songplay_table_create = """
+CREATE TABLE IF NOT EXISTS songplays (
+    songplay_id int IDENTITY (0,1),
+    start_time timestamp not null,
+    user_id int,
+    level text not null,
+    song_id text not null,
+    artist_id text not null,
+    session_id int,
+    location text not null,
+    user_agent text,
+    primary key(songplay_id)
+)
 """
 
 user_table_create = """
+CREATE TABLE IF NOT EXISTS users (
+    user_id int IDENTITY(0,1),
+    first_name text not null,
+    last_name text not null,
+    gender text not null,
+    level text not null,
+    primary key(user_id)
+)
 """
 
 song_table_create = """
+CREATE TABLE IF NOT EXISTS songs (
+    song_id text,
+    title text not null,
+    artist_id text not null,
+    year int not null,
+    duration decimal not null,
+    primary key(song_id)
+)
 """
 
 artist_table_create = """
+CREATE TABLE IF NOT EXISTS artists (
+    artist_id text,
+    name text not null,
+    location text,
+    latitude decimal,
+    longitude decimal,
+    primary key(artist_id)
+)
 """
 
 time_table_create = """
+CREATE TABLE IF NOT EXISTS time (
+    start_time timestamp not null,
+    hour int not null,
+    day int not null,
+    week int not null,
+    month int not null,
+    year int not null,
+    weekday int not null
+)
 """
 
 # STAGING TABLES
 
 staging_events_copy = (
     """
+COPY staging_events
+FROM 's3://udacity-dend/log_data'
+credentials 'aws_iam_role={}'
+format as json 'auto'
 """
-).format()
+).format("arn:aws:iam::071889381944:role/myRedshiftRole")
 
 staging_songs_copy = (
     """
+COPY staging_songs
+FROM 's3://udacity-dend/song_data'
+credentials 'aws_iam_role={}'
+format as json 'auto'
 """
-).format()
+).format("arn:aws:iam::071889381944:role/myRedshiftRole")
 
 # FINAL TABLES
 
 songplay_table_insert = """
+INSERT INTO songplays (
+    start_time,
+    user_id,
+    level,
+    song_id,
+    artist_id,
+    session_id,
+    location,
+    user_agent
+) ( 
+    SELECT 
+        (timestamp 'epoch' + a.ts::numeric / 1000 * interval '1 second'),
+        a.userid,
+        a.level,
+        b.song_id,
+        b.artist_id,
+        a.sessionid,
+        a.location,
+        a.useragent
+    FROM staging_events a
+    LEFT JOIN staging_songs b
+    ON a.artist = b.artist_name
+    and a.song = b.title
+    where b.song_id is not null
+)
 """
 
 user_table_insert = """
+INSERT INTO users (
+    first_name,
+    last_name,
+    gender,
+    level 
+) (
+    SELECT 
+        firstname,
+        lastname,
+        gender,
+        level
+    FROM staging_events
+    where firstname is not null
+)
 """
 
 song_table_insert = """
+INSERT INTO songs (
+    song_id,
+    title,
+    artist_id,
+    year,
+    duration
+) (
+    SELECT
+        song_id,
+        title,
+        artist_id,
+        year,
+        duration
+    FROM staging_songs
+)
 """
 
 artist_table_insert = """
+INSERT INTO artists (
+    artist_id,
+    name,
+    location,
+    latitude,
+    longitude
+) (
+    SELECT
+        artist_id,
+        artist_name,
+        artist_location,
+        artist_latitude,
+        artist_longitude
+    FROM staging_songs
+)
 """
 
 time_table_insert = """
+INSERT INTO time (
+    start_time,
+    hour,
+    day,
+    week,
+    month,
+    year,
+    weekday
+) (
+    SELECT
+        (timestamp 'epoch' + ts::numeric / 1000 * interval '1 second'),
+        EXTRACT (HOUR FROM (timestamp 'epoch' + ts::numeric / 1000 * interval '1 second')),
+        EXTRACT (DAY FROM (timestamp 'epoch' + ts::numeric / 1000 * interval '1 second')),
+        EXTRACT (WEEK FROM (timestamp 'epoch' + ts::numeric / 1000 * interval '1 second')),
+        EXTRACT (MONTH FROM (timestamp 'epoch' + ts::numeric / 1000 * interval '1 second')),
+        EXTRACT (YEAR FROM (timestamp 'epoch' + ts::numeric / 1000 * interval '1 second')),
+        EXTRACT (DAYOFWEEK FROM (timestamp 'epoch' + ts::numeric / 1000 * interval '1 second'))
+    FROM staging_events
+)
 """
 
 # QUERY LISTS
